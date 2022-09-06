2583aa89a99a3a05424511ec56a27df525ae9b31
Jaswant Chaudhary <91217491+jchy@users.noreply.github.com>
2022-09-06 11:46:29 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Calculate Students Total Fee</h1>
+      <h2>Weclsome to Fee Calculator</h2>
+    </div>
+  );
+}
